ccde0b91db7b8a6879729b73edd783a9a480d79b
Atul Kesharwani <148064841+Atul7307@users.noreply.github.com>
2025-04-14 14:53:02 +0530

    adding url

--- Frontend/src/utils/data.js
+++ Frontend/src/utils/data.js
@@ -1,4 +1,10 @@
-export const USER_API_ENDPOINT = "http://localhost:5011/api/user";
-export const JOB_API_ENDPOINT = "http://localhost:5011/api/job";
-export const APPLICATION_API_ENDPOINT = "http://localhost:5011/api/application";
-export const COMPANY_API_ENDPOINT = "http://localhost:5011/api/company";
+// export const USER_API_ENDPOINT = "http://localhost:5011/api/user";
+// export const JOB_API_ENDPOINT = "http://localhost:5011/api/job";
+// export const APPLICATION_API_ENDPOINT = "http://localhost:5011/api/application";
+// export const COMPANY_API_ENDPOINT = "http://localhost:5011/api/company";
+
+
+export const USER_API_ENDPOINT = "https://jobportal-backend-rvvk.onrender.com/api/user";
+export const JOB_API_ENDPOINT = "https://jobportal-backend-rvvk.onrender.com/api/job";
+export const APPLICATION_API_ENDPOINT = "https://jobportal-backend-rvvk.onrender.com/api/application";
+export const COMPANY_API_ENDPOINT = "https://jobportal-backend-rvvk.onrender.com/api/company";
